3f86b5f39a9a0d9b3bcacba0788cc44a9c926ba8
Dorin Ionita <dorin.ionita@nxp.com>
2019-10-29 15:18:35 +0200

    .

--- serial_wave/src/main.rs
+++ serial_wave/src/main.rs
@@ -29,9 +29,13 @@ struct Scenario {
 struct Simulation {
     scenaries   : Vec<Scenario>,
     scenario_idx: i32,
+    num_scenarios: i32,
 }
 
-fn import_data(filename : String) {
+static MAX_SCENARIOS : i32 = 10;
+
+fn import_data(filename : String, mut sim : Simulation) -> Simulation 
+{
     let file_content     = fs::read_to_string(filename).expect("Couldn't read from input file");
     
     let mut file_lines   = file_content.split("\n");
@@ -41,12 +45,42 @@ fn import_data(filename : String) {
         // println!("New split is {:?}", new_split);
         match new_split.len(){
             2 => {
-                    let (meaning, value) = (new_split[0], new_split[1]);
-                    println!("meaning {}, value {}", meaning, value);
+                    let (meaning, value) : (&str, &str) = (new_split[0].as_ref(), new_split[1]);
+
+                    println!("meaning-{}, value-{}", meaning, value);
+
+                    match meaning{
+                        "[NUM_SCENARIOS]" => {
+                                let value : String         = value.chars().filter(|c| !c.is_whitespace()).collect();
+                                let num_value : i32 = value.parse::<i32>().unwrap();
+                                sim.num_scenarios = if num_value  > MAX_SCENARIOS { MAX_SCENARIOS } else { num_value };
+                                println!("Managed to parse");
+                        },
+                        "[SCENARIO]"      => println!("Scenario"),
+                        "[SIZEX]"         => println!("sizex"),
+                        "[SIZEY]"         => println!("sizey"),
+                        "[H]"             => println!("h"),
+                        "[MAX_TIME]"      => println!("max_time"),
+                        "[TIME_STEP]"     => println!("time step"),
+                        "[SAVE_TIME]"     => println!("save time"),
+                        "[SRC_X]"         => println!("src x"),
+                        "[SRC_Y]"         => println!("src y"),
+                        "[SRC_RADIUS]"    => println!("src radius"),
+                        "[SRC_AMPLITUDE]" => println!("src ampl"),
+                        "[NUM_STRUCTURES]"=> println!("num structures"),
+                        "[STRUCTURE]"     => println!("structure"),
+                        "[P1]"            => println!("p1"),
+                        "[P2]"            => println!("p2"),
+                        "[P3]"            => println!("p3"),
+                        "[P4]"            => println!("p4"),
+                        _                 => (),
+                    }
             },
             _ => (),
         }
     }
+
+    sim
     // println!("File content is {}", file_content);
     // for line in file_contents.lines(){
         
@@ -55,7 +89,12 @@ fn import_data(filename : String) {
 
 fn main()
 {
-    let path = String::from("/home/dorin/temapp/serial_wave/input");
-    import_data(path);
-    println!("/home/dorin/temapp/serial_wave/input");
+    let path = String::from("/home/dorin/rust/serial_wave/input");
+    let mut sim = Simulation {
+        scenaries : Vec::new(),
+        scenario_idx : 0,
+        num_scenarios : 0,
+    };
+    import_data(path, sim);
+    println!("/home/dorin/rust/serial_wave/input");
 }
---
+++ serial_wave/target/debug/deps/serial_wave-d944edcc4f57e746.d
@@ -0,0 +1,5 @@
+/home/dorin/rust/serial_wave/target/debug/deps/serial_wave-d944edcc4f57e746: src/main.rs
+
+/home/dorin/rust/serial_wave/target/debug/deps/serial_wave-d944edcc4f57e746.d: src/main.rs
+
+src/main.rs:
